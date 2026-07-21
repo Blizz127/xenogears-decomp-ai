be7f358234bb332a878732e48e9a9deb78da51c9
blizz <blizz@users.noreply.github.com>
2026-07-20 23:26:24 -0500

    fix(port): include_asm.h honors SKIP_ASM to neutralize INCLUDE_ASM in the native build

--- include/include_asm.h
+++ include/include_asm.h
@@ -1,41 +1,43 @@
-#ifndef _INCLUDE_ASM_H
-#define _INCLUDE_ASM_H
+#ifndef INCLUDE_ASM_H
+#define INCLUDE_ASM_H
 
-#if !defined(SPLAT) && !defined(M2CTX) && !defined(PERMUTER) && !defined(SKIP_ASM)
+#if !defined(M2CTX) && !defined(PERMUTER) && !defined(SKIP_ASM)
 
-        #ifndef INCLUDE_ASM
-                #define INCLUDE_ASM(FOLDER, NAME)                                                    \
-                void __maspsx_include_asm_hack_##NAME() { \
-                        __asm__( \
-                            ".text # maspsx-keep \n" \
-                            "\t.align\t2 # maspsx-keep\n" \
-                            "\t.set noreorder # maspsx-keep\n" \
-                            "\t.set noat # maspsx-keep\n" \
-                            ".include \""FOLDER"/"#NAME".s\" # maspsx-keep\n" \
-                            "\t.set reorder # maspsx-keep\n" \
-                            "\t.set at # maspsx-keep\n" \
-                        ); \
-                    }
-        #endif
+#ifndef INCLUDE_ASM
+#define INCLUDE_ASM(FOLDER, NAME) \
+    __asm__( \
+        ".section .text\n" \
+        "    .set noat\n" \
+        "    .set noreorder\n" \
+        "    .include \"" FOLDER "/" #NAME ".s\"\n" \
+        "    .set reorder\n" \
+        "    .set at\n" \
+    )
+#endif
+#ifndef INCLUDE_RODATA
+#define INCLUDE_RODATA(FOLDER, NAME) \
+    __asm__( \
+        ".section .rodata\n" \
+        "    .include \"" FOLDER "/" #NAME ".s\"\n" \
+        ".section .text" \
+    )
+#endif
 
-        #ifndef INCLUDE_RODATA
-                #define INCLUDE_RODATA(FOLDER, NAME)                                                 \
-                        __asm__(".section .rodata\n"                                                 \
-                                "\t.include \"" FOLDER "/" #NAME ".s\"\n"                            \
-                                ".section .text")
-        #endif
-
-        __asm__(".include \"macro.inc\"\n"
-                "\t.set\treorder\n"
-                "\t.set\tat\n");
+#if INCLUDE_ASM_USE_MACRO_INC
+__asm__(".include \"include/macro.inc\"\n");
+#else
+__asm__(".include \"include/labels.inc\"\n");
+#endif
 
 #else
-        #ifndef INCLUDE_ASM
-                #define INCLUDE_ASM(FOLDER, NAME)
-        #endif
-        #ifndef INCLUDE_RODATA
-                #define INCLUDE_RODATA(FOLDER, NAME)
-        #endif
+
+#ifndef INCLUDE_ASM
+#define INCLUDE_ASM(FOLDER, NAME)
+#endif
+#ifndef INCLUDE_RODATA
+#define INCLUDE_RODATA(FOLDER, NAME)
 #endif
 
-#endif
+#endif /* !defined(M2CTX) && !defined(PERMUTER) */
+
+#endif /* INCLUDE_ASM_H */
