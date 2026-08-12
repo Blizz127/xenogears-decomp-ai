f652c44a099a35ef2d4e33246a81d36b9f4978fe
blizz <blizz@users.noreply.github.com>
2026-08-12 04:05:20 +0000

    Implement world callback 0x80092BE4

--- pc_port/build_port.sh
+++ pc_port/build_port.sh
@@ -1059,6 +1059,7 @@ PORT_SOURCES=(
     pc_port/src/world_map_callback_91430.c
     pc_port/src/world_map_callback_91b54.c
     pc_port/src/world_map_callback_92234.c
+    pc_port/src/world_map_callback_92be4.c
     pc_port/src/world_map_helper_848b4.c
     pc_port/src/world_map_helper_c28c.c
     pc_port/src/world_map_helper_c364.c
--- pc_port/src/world_map_scheduler.c
+++ pc_port/src/world_map_scheduler.c
@@ -49,6 +49,7 @@
 #include "world_map_callback_91430.h"
 #include "world_map_callback_91b54.h"
 #include "world_map_callback_92234.h"
+#include "world_map_callback_92be4.h"
 #include "world_map_scheduler.h"
 
 /* Focused legacy scheduler tests intentionally link the scheduler without
@@ -67,6 +68,7 @@ extern s32 wm_800906E0(s32 slot_index) __attribute__((weak));
 extern s32 wm_80091430(s32 slot_index) __attribute__((weak));
 extern s32 wm_80091B54(s32 slot_index) __attribute__((weak));
 extern s32 wm_80092234(s32 slot_index) __attribute__((weak));
+extern s32 wm_80092BE4(s32 slot_index) __attribute__((weak));
 
 #define WM_SCHED_RAM(a) ((u8*)PSX_ADDR(a))
 
@@ -230,6 +232,11 @@ static s16 wm_sched_builtin_80092234(int slot_index)
     return (s16)wm_80092234((s32)slot_index);
 }
 
+static s16 wm_sched_builtin_80092BE4(int slot_index)
+{
+    return (s16)wm_80092BE4((s32)slot_index);
+}
+
 static int wm_sched_is_known_missing(u32 guest_addr)
 {
     unsigned i;
@@ -295,6 +302,10 @@ static wm_sched_cb_resolve_t wm_sched_resolve(u32 guest_addr,
     }
     if (guest_addr == 0x80092234u && wm_80092234 != 0) {
         *out_fn = wm_sched_builtin_80092234;
+        return WM_SCHED_CB_IMPLEMENTED;
+    }
+    if (guest_addr == 0x80092BE4u && wm_80092BE4 != 0) {
+        *out_fn = wm_sched_builtin_80092BE4;
         return WM_SCHED_CB_IMPLEMENTED;
     }
     if (wm_sched_is_known_missing(guest_addr))
