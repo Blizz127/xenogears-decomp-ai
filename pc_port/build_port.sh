cd19fb6cd9a874490d15c3c2dcefa5e466bbe8e0
blizz <blizz@users.noreply.github.com>
2026-08-14 21:51:06 +0000

    Rebase world helper 0x80074794

--- pc_port/build_port.sh
+++ pc_port/build_port.sh
@@ -1072,6 +1072,7 @@ PORT_SOURCES=(
     pc_port/src/world_map_helper_8e034.c
     pc_port/src/world_map_helper_73b04.c
     pc_port/src/world_map_helper_90a84.c
+    pc_port/src/world_map_helper_74794.c
 )
 
 # The list above is the port link's explicit ownership registry. Refuse to
