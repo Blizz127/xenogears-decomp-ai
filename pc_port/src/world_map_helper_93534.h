5ebea5bff76186758917abba8e7de1dd7d97e44c
blizz <blizz@users.noreply.github.com>
2026-08-15 01:30:13 +0000

    Implement world helper 0x80093534

---
+++ pc_port/src/world_map_helper_93534.h
@@ -0,0 +1,14 @@
+/*
+ * World-map signed X/Z wrap helper 0x80093534.
+ *
+ * Retail boundary: [0x80093534, 0x800935DC), 168 bytes / 42
+ * instructions in world_map.bin loaded at 0x8006FAF0.
+ */
+#ifndef WORLD_MAP_HELPER_93534_H
+#define WORLD_MAP_HELPER_93534_H
+
+#include "common.h"
+
+void wm_80093534(u32 vec_addr);
+
+#endif /* WORLD_MAP_HELPER_93534_H */
