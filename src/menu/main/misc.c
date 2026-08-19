3fde2843d7d45e9e2febea92787ac2fdf4291f7f
blizz <blizz@users.noreply.github.com>
2026-08-19 00:32:33 +0000

    Decompile func_801C85F8 (menu table NOT lookup)
    Returns bitwise NOT of D_801E96C8[idx].

--- src/menu/main/misc.c
+++ src/menu/main/misc.c
@@ -1088,7 +1088,9 @@ u16 func_801C85DC(u8 idx) {
     return D_801E96A8[idx];
 }
 
-INCLUDE_ASM("../asm/menu/nonmatchings/main/misc", func_801C85F8);
+u16 func_801C85F8(u8 idx) {
+    return (u16)(~D_801E96C8[idx]);
+}
 
 INCLUDE_ASM("../asm/menu/nonmatchings/main/misc", func_801C861C);
 
