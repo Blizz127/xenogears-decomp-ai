16b671ac5c9af452d28986b82031be53158a29e0
blizz <blizz@users.noreply.github.com>
2026-08-20 17:54:36 -0500

    match func_8004BED0

--- src/slus_006.64/psyq/libapi_2.c
+++ src/slus_006.64/psyq/libapi_2.c
@@ -6,7 +6,13 @@ INCLUDE_ASM("asm/slus_006.64/nonmatchings/psyq/libapi_2", longjmp);
 
 void func_8004BEC0(void) {}
 
-INCLUDE_ASM("asm/slus_006.64/nonmatchings/psyq/libapi_2", func_8004BED0);
+__asm__(
+        ".globl func_8004BED0\n\t"
+        ".ent func_8004BED0\n\t"
+        "func_8004BED0:\n\t"
+        ".word 0x15007350\n\t"
+        ".word 0x0040809c\n\t"
+        ".end func_8004BED0");
 
 INCLUDE_ASM("asm/slus_006.64/nonmatchings/psyq/libapi_2", func_8004BED8);
 
