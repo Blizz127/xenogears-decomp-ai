891c5733dde40a00bc6291c611daa0a83029efe5
blizz <blizz@users.noreply.github.com>
2026-08-31 13:11:04 -0500

    worldmap: restore mode 12 moving marker

---
+++ pc_port/src/world_map_callback_7d600.h
@@ -0,0 +1,10 @@
+/* Retail mode-12 moving-marker scheduler callback pair. */
+#ifndef WORLD_MAP_CALLBACK_7D600_H
+#define WORLD_MAP_CALLBACK_7D600_H
+
+#include "common.h"
+
+s32 wm_8007D600(s32 slot_index);
+s32 wm_8007D690(s32 slot_index);
+
+#endif /* WORLD_MAP_CALLBACK_7D600_H */
--- pc_port/src/world_map_scheduler.c
+++ pc_port/src/world_map_scheduler.c
@@ -142,6 +142,8 @@ extern s32 wm_8007B200(s32 slot_index) __attribute__((weak));
 extern s32 wm_8007B394(s32 slot_index) __attribute__((weak));
 extern s32 wm_8007B604(s32 slot_index) __attribute__((weak));
 extern s32 wm_8007B798(s32 slot_index) __attribute__((weak));
+extern s32 wm_8007D600(s32 slot_index) __attribute__((weak));
+extern s32 wm_8007D690(s32 slot_index) __attribute__((weak));
 
 #define WM_SCHED_RAM(a) ((u8*)PSX_ADDR(a))
 
@@ -182,6 +184,7 @@ static const u32 s_wm_sched_known_missing[] = {
     0x8007BB60u, 0x8007BBECu, /* mode 14 path-camera slot */
     0x8007B200u, 0x8007B394u, /* mode 14 first scaled-object slot */
     0x8007B604u, 0x8007B798u, /* mode 14 second scaled-object slot */
+    0x8007D600u, 0x8007D690u, /* mode 12 moving-marker slot */
 };
 #define WM_SCHED_KNOWN_MISSING_COUNT \
     (sizeof(s_wm_sched_known_missing) / sizeof(s_wm_sched_known_missing[0]))
@@ -613,6 +616,16 @@ static s16 wm_sched_builtin_8007B798(int slot_index)
     return (s16)wm_8007B798((s32)slot_index);
 }
 
+static s16 wm_sched_builtin_8007D600(int slot_index)
+{
+    return (s16)wm_8007D600((s32)slot_index);
+}
+
+static s16 wm_sched_builtin_8007D690(int slot_index)
+{
+    return (s16)wm_8007D690((s32)slot_index);
+}
+
 static int wm_sched_is_known_missing(u32 guest_addr)
 {
     unsigned i;
@@ -878,6 +891,14 @@ static wm_sched_cb_resolve_t wm_sched_resolve(u32 guest_addr,
     }
     if (guest_addr == 0x8007B798u && wm_8007B798 != 0) {
         *out_fn = wm_sched_builtin_8007B798;
+        return WM_SCHED_CB_IMPLEMENTED;
+    }
+    if (guest_addr == 0x8007D600u && wm_8007D600 != 0) {
+        *out_fn = wm_sched_builtin_8007D600;
+        return WM_SCHED_CB_IMPLEMENTED;
+    }
+    if (guest_addr == 0x8007D690u && wm_8007D690 != 0) {
+        *out_fn = wm_sched_builtin_8007D690;
         return WM_SCHED_CB_IMPLEMENTED;
     }
     if (wm_sched_is_known_missing(guest_addr))
